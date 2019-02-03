4b671b74ff5c4351e808c4a49b240fa1f35873d7
brandonlou <brandonlou08@gmail.com>
2019-02-02 21:50:11 -0800

    Simplified vision code

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # CardinalVision
-This is a experimental project contributing to the over all image recognition of the 2019 robot.
-Be sure to install opencv-python and python3 before running
+This is Team 4159's image recognition code running on the Nvidia Jetson TX1. We use an IR sensitive camera with an IR filter and an IR LED ring around it to detect the retroreflective tape.
 
 # How to install and run:
 1. Install python
 2. Clone this directory
-3. CD into directory
-4. "pip install requirements.txt"
-
-Note:
-
-- Press q to exit all windows when running
-- Please run TapeRec.py for FRC Visual target recognition
-- Run VisionTesting.py for pencil detection
+3. `cd` into directory
+4. `pip install requirements.txt`
+5. `python3 Vision.py`
---
+++ Vision.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+
+
+# Takes a contour and fits a line to it. Returns 2 points on that line.
+def fitLine(cnt):
+    rows, cols = img.shape[:2]
+    [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+    lefty = int((-x*vy/vx) + y)
+    righty = int(((cols-x)*vy/vx)+y)
+    x0 = cols-1
+    y0 = righty
+    x1 = 0
+    y1 = lefty
+    return x0, y0, x1, y1
+
+
+# Takes a list of contours and returns it sorted from left to right. TODO: This can be simplified...
+def sortLeftToRight(cnts):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+    
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b:b[1][i], reverse=reverse))
+
+    # return the list of sorted contours and bounding boxes
+    return cnts
+
+
+# Takes two rectangles in the type of a tuple (x, y, w, h) and returns a larger rectangle in the same format that encompasses both of them
+def union(a,b):
+    x = min(a[0], b[0])
+    y = min(a[1], b[1])
+    w = max(a[0]+a[2], b[0]+b[2]) - x
+    h = max(a[1]+a[3], b[1]+b[3]) - y
+    return (x, y, w, h)
+
+
+# Uncomment to use a video stream
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 320) theoretically you can set the camera properties
+# cap.set(4, 240)
+# while(1):
+# Capture frame-by-frame
+#_, frame = cap.read()
+
+
+# For testing purposes we process on an image
+img = cv2.imread('test.png')
+
+# Convert BGR to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Define range of threshold in HSV
+lower_range = np.array([0.0, 0.0, 255.0])
+upper_range = np.array([0.0, 0.0, 255.0])
+
+# Threshold HSV to only get white target
+mask = cv2.inRange(hsv, lower_range, upper_range)
+
+# Find contours
+_, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+# Sort contours from left to right
+contours = sortLeftToRight(contours)
+
+# Loop through each contour(tape) with an index
+for idx, tape in enumerate(contours):
+    
+    # Only loop until second to last contour
+    if(idx < len(contours) - 1):
+        
+        # Get the current indexed contour and next index contour in the list
+        leftTape = contours[idx]
+        rightTape = contours[idx+1]
+        
+        # Get the Moment objects of the two contours
+        leftM = cv2.moments(leftTape)
+        rightM = cv2.moments(rightTape)
+        
+        # Calculate the centeroid y coordinates of the two contours
+        leftY = int(leftM['m01']/leftM['m00'])
+        rightY = int(rightM['m01']/rightM['m00'])
+        
+        # Fit each contour to a line and get two points from each line
+        x1, y1, x2, y2 = fitLine(leftTape)
+        x3, y3, x4, y4 = fitLine(rightTape)
+
+        # Compute the y coordinate of the intersection point of the two lines
+        intersectY = y1 + ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1)) * (y2-y1)
+
+        # Run if the intersection of the two lines are higher than the contours (the two pieces of tape are angled towards each other)
+        if(intersectY < rightY):
+            
+            # Bound a rectangle on the two contours and get the (x,y) of the top left coordinate and the rectangle's width and height
+            xL, yL, wL, hL = cv2.boundingRect(leftTape)
+            xR, yR, wR, hR = cv2.boundingRect(rightTape)
+            
+            # Assign x, y, width, and height to a tuple that represents a rectangle
+            leftRect = (xL,yL,wL,hL)
+            rightRect = (xR,yR,wR,hR)
+            
+            # Combine the two rectangles by computing its union (creating a bounding box around the two pieces of tape)
+            combinedRect = union(leftRect, rightRect)
+            
+            # Draw the bounding box to the frame
+            img = cv2.rectangle(img, (combinedRect[0], combinedRect[1]), (combinedRect[0]+combinedRect[2], combinedRect[1]+combinedRect[3]), (0,255,0), 3)
+
+            # TODO: Find bounding box with largest area
+
+
+# Display the frame
+cv2.imshow("Frame", img)
+
+# Exit program if any key is pressed
+cv2.waitKey(0)
+
+
+# Uncomment when using a video stream
+# When everything done, release the capture
+# cap.release()
+# cv2.destroyAllWindows()

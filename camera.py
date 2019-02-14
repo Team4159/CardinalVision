a28e69edf4a9411033561c23c87af0ab95b7afe0
Kai Chang <kaijchang@gmail.com>
2019-02-14 08:22:26 -0800

    rewrite with sanic for asyncio

--- camera.py
+++ camera.py
@@ -1,41 +1,25 @@
 # https://github.com/log0/video_streaming_with_flask_example/blob/master/camera.py
 
 import cv2
-import zmq
 
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 2 and 3.
+        # Using OpenCV to capture from device 0 and 1.
         self.video0 = cv2.VideoCapture(0)
         self.video1 = cv2.VideoCapture(1)
 
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        self.socket.connect('tcp://127.0.0.1:5803')  # arbitrary
-        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
-
-        self.switch = False
-        self.last_value = 0
-
-    def __del__(self):
+    def release(self):
         self.video0.release()
         self.video1.release()
 
-    def get_frame(self):
-        value = int(self.socket.recv())
-
-        if value and not self.last_value:
-            self.switch = not self.switch
-
-        self.last_value = value
-
-        if self.switch:
-            success, image = self.video1.read()
+    def get_frame(self, camera):
+        if camera == 0:
+            success, image = self.video0.read()
             image = cv2.applyColorMap(image, cv2.COLORMAP_PINK)
 
-        else:
-            success, image = self.video0.read()
+        elif camera == 1:
+            success, image = self.video1.read()
             image = cv2.applyColorMap(image, cv2.COLORMAP_BONE)
 
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
--- stream_server.py
+++ stream_server.py
@@ -1,38 +1,58 @@
 # https://github.com/log0/video_streaming_with_flask_example/blob/master/main.py
 
-from flask import Flask, render_template, Response
+from sanic import Sanic
+from sanic.response import stream, html
 from camera import VideoCamera
+import zmq.asyncio
+import asyncio
 
 import time
 
 
-class StreamServer(Flask):
+class StreamServer(Sanic):
     def __init__(self, *args, **kwargs):
-        # looping
-        self.last_tick = time.time()
-        self.tick_time = 1 / 60  # 60 fps
+        Sanic.__init__(self, *args, **kwargs)
 
-        Flask.__init__(self, *args, **kwargs)
+        self.context = zmq.asyncio.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect('tcp://127.0.0.1:5803')  # arbitrary
+        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
 
         @self.route('/')
-        def index():
-            return render_template('index.html')
+        def index(request):
+            return html('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Driver Cam</title>
+</head>
+<body>
+    <img id="bg" src="/video_feed" alt="Driver Cam">
+</body>
+</html>            
+            ''')
 
-        @self.route('/video_feed')
-        def video_feed():
-            return Response(self.make_stream(VideoCamera()),
-                            mimetype='multipart/x-mixed-replace; boundary=frame')
+        @self.route('/video_feed', stream=True)
+        def video_feed(request):
+            return stream(self.make_stream,
+                          content_type='multipart/x-mixed-replace; boundary=frame')
 
-    def make_stream(self, camera):
+    async def make_stream(self, response):
+        camera = VideoCamera()
+        tick_time = 1 / 60  # 60 fps
+        last_tick = time.time()
+        loop = asyncio.get_event_loop()
+
         while True:
             tmp = time.time()
-            if self.tick_time - (time.time() - self.last_tick) > 0:
-                time.sleep(self.tick_time - (time.time() - self.last_tick))
-            self.last_tick = tmp
+            if tick_time - (time.time() - last_tick) > 0:
+                await asyncio.sleep(tick_time - (time.time() - last_tick))
+            last_tick = tmp
 
-            frame = camera.get_frame()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+            frame = await loop.run_in_executor(None, camera.get_frame, 0)
+            response.write(b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 # Use UDP/TCP ports 5800 - 5810 only. Set debug to False in competition.
